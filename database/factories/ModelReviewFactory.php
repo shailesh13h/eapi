25947323aac6139d4c6bd024155e14060f0918db
shailesh <shailesh13h@gmail.com>
2018-02-28 14:24:26 +0530

    Modified Product Details

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -20,7 +20,7 @@ $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
         'name'=>$faker->word,
          'details'=>$faker->paragraph,
-        'price'=>$faker->numberBetween(min(100,1000)),
+        'price'=>$faker->numberBetween(100,1000),
         'stock'=>$faker->randomDigit,
 
         'discount'=>$faker->numberBetween(2,40),

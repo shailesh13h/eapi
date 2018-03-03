1c9a8084ff80218670abd10d50986a02588be885
shailesh <shailesh13h@gmail.com>
2018-03-03 16:34:04 +0530

    Handle the Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,9 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use  \Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -17,7 +20,7 @@ class Handler extends ExceptionHandler
         \Illuminate\Auth\AuthenticationException::class,
         \Illuminate\Auth\Access\AuthorizationException::class,
         \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
     ];
@@ -44,6 +47,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        if($request->expectsJson()){
+
+            if($exception instanceof ModelNotFoundException){
+                return response(['errors'=>'Product Model Not Found'],Response::HTTP_NOT_FOUND);
+            }
+        }
+
+
+            if($exception instanceof NotFoundHttpException){
+                return response(['errors'=>'Incorect route'],Response::HTTP_NOT_FOUND);
+            }
+
+//die nd dump DD
+        //dd($exception);
+
         return parent::render($request, $exception);
     }
 
